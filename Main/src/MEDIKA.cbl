fde2c9d2ce2c44b073ddff272948fb39cc93702d
zelly <116586070+chi4ki1@users.noreply.github.com>
2024-11-18 18:26:12 +0800

    update MEDIKA

--- Main/src/MEDIKA.cbl
+++ Main/src/MEDIKA.cbl
@@ -8,7 +8,7 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT PATIENT-FILE ASSIGN TO
-           "\wsl.localhost\Debian\home\zelly\DATA\PATIENT.txt\"
+           "/home/zelly/PATIENTS.dat"
                ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
@@ -19,17 +19,18 @@
        WORKING-STORAGE SECTION.
        01  USER-CHOICE PIC X.
        01  EOF-FLAG PIC X VALUE "N".
+       01  FILE-STATUS PIC XX.
        01  SEARCH-NAME PIC X(30).
        01  WS-PATIENT-NAME PIC X(30).
-       01  WS-STUDENT-NUMBER PIC X(10).
+       01  WS-STUDENT-NUMBER PIC X(15).
        01  WS-STUDENT-CYS PIC X(10).
        01  WS-DATE-OF-BIRTH PIC X(10).
        01  WS-PATIENT-AGE PIC 99.
-       01  WS-PATIENT-SEX PIC A(1).
+       01  WS-PATIENT-SEX PIC X(1).
        01  WS-HEALTH-CONDITION PIC X(30).
        01  WS-DATE-OF-VISIT PIC X(10).
        01  NEW-LINE PIC X(1) VALUE X'0A'.
-       01  FRA-ME PIC X(35) VALUE "===================================".        *> for main header terminal-based design purposes
+       01  FRA-ME PIC X(35) VALUE "===================================".*> for main header terminal-based design purposes
       *
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
@@ -38,12 +39,10 @@
            DISPLAY FRA-ME.
            DISPLAY "            MAIN MENU".
            DISPLAY FRA-ME.
-           DISPLAY NEW-LINE.
            DISPLAY "Good Day, Admin!".
            DISPLAY "What would you like to do?".
            PERFORM DISPLAY-MENU.
            DISPLAY NEW-LINE
-           DISPLAY "Enter a letter to proceed: " WITH NO ADVANCING
            PERFORM UNTIL USER-CHOICE = "x" OR USER-CHOICE = "X"
                ACCEPT USER-CHOICE
                EVALUATE USER-CHOICE
@@ -75,38 +74,60 @@
            DISPLAY "c) Search Patient Record".
            DISPLAY "d) Update Patient Info".
            DISPLAY "x) Exit Program".
+           DISPLAY "Enter a letter to proceed: " WITH NO ADVANCING.
       *
        ADD-PATIENT.
            OPEN EXTEND PATIENT-FILE.
+           MOVE "00" TO FILE-STATUS.
+
+           IF FILE-STATUS NOT = "00"
+               DISPLAY "Error opening file."
+               EXIT PROGRAM
+           END-IF.
+
            DISPLAY FRA-ME.
            DISPLAY "      MEDIKA PATIENT RECORD".
            DISPLAY FRA-ME.
            DISPLAY "        ADD PATIENT RECORD".
            DISPLAY FRA-ME.
-           DISPLAY "Enter Student ID Number: ".
+
+           DISPLAY "Student ID Number: " WITH NO ADVANCING.
            ACCEPT WS-STUDENT-NUMBER.
-           DISPLAY "Enter Student Name: ".
+
+           DISPLAY "Name (FN, MI, LN): " WITH NO ADVANCING.
            ACCEPT WS-PATIENT-NAME.
-           DISPLAY "Enter Student Course/Section: ".
+
+           DISPLAY "Course/Section: " WITH NO ADVANCING.
            ACCEPT WS-STUDENT-CYS.
-           DISPLAY "Enter Student Date of Birth (yyyy/mm/dd): ".
+
+           DISPLAY "Date of Birth (yyyy/mm/dd): " WITH NO ADVANCING.
            ACCEPT WS-DATE-OF-BIRTH.
-           DISPLAY "Enter Student Age: ".
+
+           DISPLAY "Age: " WITH NO ADVANCING.
            ACCEPT WS-PATIENT-AGE.
-           DISPLAY "Enter Student Assigned Sex at Birth: ".
+
+           DISPLAY "Assigned Sex at Birth: " WITH NO ADVANCING.
            ACCEPT WS-PATIENT-SEX.
-           DISPLAY "Enter Student Health Condition: ".
+
+           DISPLAY "Enter Student Health Condition: " WITH NO ADVANCING.
            ACCEPT WS-HEALTH-CONDITION.
-           DISPLAY "Enter Date of Visit (yyyy/mm/dd): ".
+
+           DISPLAY "Date of Visit (yyyy/mm/dd): " WITH NO ADVANCING.
            ACCEPT WS-DATE-OF-VISIT.
+
            DISPLAY FRA-ME.
            DISPLAY "Add New Record? (Y/N): " WITH NO ADVANCING.
            ACCEPT USER-CHOICE.
-           MOVE WS-PATIENT-NAME TO PATIENT-RECORD(1:30).
-           MOVE WS-STUDENT-NUMBER TO PATIENT-RECORD(31:10).
-           MOVE WS-HEALTH-CONDITION TO PATIENT-RECORD(41:30).
-           MOVE WS-DATE-OF-VISIT TO PATIENT-RECORD(71:10).
-           WRITE PATIENT-RECORD.
+
+           IF USER-CHOICE = "Y"
+               MOVE WS-PATIENT-NAME TO PATIENT-RECORD(1:30)
+               MOVE WS-STUDENT-NUMBER TO PATIENT-RECORD(31:10)
+               MOVE WS-HEALTH-CONDITION TO PATIENT-RECORD(41:30)
+               MOVE WS-DATE-OF-VISIT TO PATIENT-RECORD(71:10)
+               WRITE PATIENT-RECORD
+           ELSE
+               DISPLAY "Record not added.".
+
            CLOSE PATIENT-FILE.
       *
        VIEW-PATIENTS.
@@ -118,6 +139,10 @@
                MOVE PATIENT-RECORD(41:30) TO WS-HEALTH-CONDITION
                MOVE PATIENT-RECORD(71:10) TO WS-DATE-OF-VISIT
                DISPLAY "Patient Name: " WS-PATIENT-NAME
+               DISPLAY "Age: " WS-PATIENT-AGE
+               DISPLAY "Sex: " WS-PATIENT-SEX
+               DISPLAY "Date of Birth: " WS-DATE-OF-BIRTH
+               DISPLAY "Course/Section: " WS-STUDENT-CYS
                DISPLAY "Student Number: " WS-STUDENT-NUMBER
                DISPLAY "Health Condition: " WS-HEALTH-CONDITION
                DISPLAY "Date of Visit: " WS-DATE-OF-VISIT
@@ -137,15 +162,21 @@
                MOVE PATIENT-RECORD(41:30) TO WS-HEALTH-CONDITION
                MOVE PATIENT-RECORD(71:10) TO WS-DATE-OF-VISIT
                IF WS-PATIENT-NAME = SEARCH-NAME THEN
-                   DISPLAY "Patient Name: " WS-PATIENT-NAME
-                   DISPLAY "Student Number: " WS-STUDENT-NUMBER
-                   DISPLAY "Health Condition: " WS-HEALTH-CONDITION
-                   DISPLAY "Date of Visit: " WS-DATE-OF-VISIT
+               DISPLAY "Patient Name: " WS-PATIENT-NAME
+               DISPLAY "Age: " WS-PATIENT-AGE
+               DISPLAY "Sex: " WS-PATIENT-SEX
+               DISPLAY "Date of Birth: " WS-DATE-OF-BIRTH
+               DISPLAY "Course/Section: " WS-STUDENT-CYS
+               DISPLAY "Student Number: " WS-STUDENT-NUMBER
+               DISPLAY "Health Condition: " WS-HEALTH-CONDITION
+               DISPLAY "Date of Visit: " WS-DATE-OF-VISIT
                    MOVE "Y" TO EOF-FLAG
                END-IF
                READ PATIENT-FILE AT END MOVE "Y" TO EOF-FLAG
            END-PERFORM.
            CLOSE PATIENT-FILE.
+      *
+
       *
        UPDATE-PATIENT.
       * (Implementation for updating patient records)
